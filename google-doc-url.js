fb4e354c7013aa08a0f1da356df7da713b240206
malejs <34074178+malejs@users.noreply.github.com>
2021-11-15 20:52:17 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,5 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1qYkfSS_PXrVMRr8xrgu19QYjtsjMaWUR4kHUp8Hx2nU/edit#gid=0';
+
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
